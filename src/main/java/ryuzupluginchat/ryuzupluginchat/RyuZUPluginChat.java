a94de5fb3297dcc8d367e8818e6813d91421f5bd
Y_RyuZU <ryuzu1006@gmail.com>
2021-06-09 19:25:18 +0900

    こみっとーさつこ

--- src/main/java/ryuzupluginchat/ryuzupluginchat/RyuZUPluginChat.java
+++ src/main/java/ryuzupluginchat/ryuzupluginchat/RyuZUPluginChat.java
@@ -65,15 +65,17 @@ public final class RyuZUPluginChat extends JavaPlugin implements PluginMessageLi
         getServer().getMessenger().registerIncomingPluginChannel(this, "ryuzuchat:ryuzuchat", this);
         Command command = new Command();
         Tell tell = new Tell();
+        Tell tell2 = new Tell();
         Reply reply = new Reply();
+        Reply reply2 = new Reply();
         Objects.requireNonNull(getCommand("rpc")).setExecutor(command);
         Objects.requireNonNull(getCommand("rpc")).setTabCompleter(command);
         Objects.requireNonNull(getCommand("tell")).setExecutor(tell);
         Objects.requireNonNull(getCommand("tell")).setTabCompleter(tell);
-        Objects.requireNonNull(getCommand("t")).setExecutor(tell);
-        Objects.requireNonNull(getCommand("t")).setTabCompleter(tell);
+        Objects.requireNonNull(getCommand("t")).setExecutor(tell2);
+        Objects.requireNonNull(getCommand("t")).setTabCompleter(tell2);
         Objects.requireNonNull(getCommand("reply")).setExecutor(reply);
-        Objects.requireNonNull(getCommand("r")).setExecutor(reply);
+        Objects.requireNonNull(getCommand("r")).setExecutor(reply2);
         getServer().getScheduler().runTaskTimerAsynchronously(ryuzupluginchat, new CheckPlayers(), 0 , 20 * 10);
         getLogger().info(ChatColor.GREEN + "RyuZUPluginChatが起動しました");
     }
