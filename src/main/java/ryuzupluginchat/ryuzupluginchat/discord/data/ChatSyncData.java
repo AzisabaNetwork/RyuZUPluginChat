f441cdf26716239c78a58cad8c1c0c73c2820002
siloneco <siloneco4646@gmail.com>
2022-01-25 12:52:51 +0900

    [Add] Discordに関するConfigの読み込み処理を追加

---
+++ src/main/java/ryuzupluginchat/ryuzupluginchat/discord/data/ChatSyncData.java
@@ -0,0 +1,14 @@
+package ryuzupluginchat.ryuzupluginchat.discord.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public abstract class ChatSyncData {
+
+  private final boolean enabled;
+  private final boolean voiceChatMode;
+  private final boolean discordInputEnabled;
+
+}
