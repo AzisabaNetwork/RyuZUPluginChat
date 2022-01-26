25c600c24285173d54e8c20e7ca67bd7e597cea4
siloneco <siloneco4646@gmail.com>
2022-01-26 13:42:24 +0900

    [Add] /tell コマンドでのTabCompleteを乗っ取り

--- src/main/java/ryuzupluginchat/ryuzupluginchat/listener/OutdatedCommandCaptureListener.java
+++ src/main/java/ryuzupluginchat/ryuzupluginchat/listener/OutdatedCommandCaptureListener.java
@@ -2,10 +2,13 @@ package ryuzupluginchat.ryuzupluginchat.listener;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.TabCompleteEvent;
 import ryuzupluginchat.ryuzupluginchat.RyuZUPluginChat;
 
 @RequiredArgsConstructor
@@ -13,7 +16,8 @@ public class OutdatedCommandCaptureListener implements Listener {
 
   private final RyuZUPluginChat plugin;
 
-  private final List<String> commands = Arrays.asList("/tell", "/reply", "/t", "/r", "/msg",
+  private final List<String> tellCommands = Arrays.asList("/tell", "/t", "/msg", "/message", "/m");
+  private final List<String> redirects = Arrays.asList("/tell", "/reply", "/t", "/r", "/msg",
       "/message", "/m");
 
   @EventHandler
@@ -24,10 +28,38 @@ public class OutdatedCommandCaptureListener implements Listener {
     }
     String label = labelAndArgs[0];
 
-    if (commands.contains(label.toLowerCase())) {
+    if (redirects.contains(label.toLowerCase())) {
       labelAndArgs[0] = "/" + plugin.getName().toLowerCase() + ":" + label.substring(1);
     }
 
     e.setMessage(String.join(" ", labelAndArgs));
+  }
+
+  @EventHandler
+  public void onTabComplete(TabCompleteEvent e) {
+    String[] split = e.getBuffer().split(" ");
+    String label = split[0];
+    if (!tellCommands.contains(label.toLowerCase())) {
+      return;
+    }
+    if (!(e.getSender() instanceof Player)) {
+      return;
+    }
+
+    final String target;
+    if (split.length > 1) {
+      target = split[1];
+    } else {
+      target = "";
+    }
+
+    Player p = (Player) e.getSender();
+
+    e.setCompletions(
+        plugin.getPlayerUUIDMapContainer().getAllNames().stream()
+            .filter(
+                l -> !l.equals(p.getName()) && l.toLowerCase().startsWith(target.toLowerCase()))
+            .collect(Collectors.toList())
+    );
   }
 }
