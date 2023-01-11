7bc673b8008df7e2a513f42997fbaa5d100bfde9
acrylic-style <me@acrylicstyle.xyz>
2023-01-11 17:49:52 +0900

    :sparkles: Prefix/Suffixの削除をできるように変更

--- src/main/java/net/azisaba/ryuzupluginchat/util/Chat.java
+++ src/main/java/net/azisaba/ryuzupluginchat/util/Chat.java
@@ -3,6 +3,7 @@ package net.azisaba.ryuzupluginchat.util;
 import java.text.MessageFormat;
 import lombok.experimental.UtilityClass;
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.Nullable;
 
 @UtilityClass
 public class Chat {
@@ -10,6 +11,13 @@ public class Chat {
   // メッセージをフォーマットして、&で色をつける
   public static String f(String text, Object... args) {
     return MessageFormat.format(ChatColor.translateAlternateColorCodes('&', text), args);
+  }
+
+  public static String translateLegacyAmpersand(@Nullable String text) {
+    if (text == null) {
+      return "null";
+    }
+    return ChatColor.translateAlternateColorCodes('&', text);
   }
 
   // 色を消す
