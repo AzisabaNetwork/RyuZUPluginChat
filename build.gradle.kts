fdb963023e3d6ef280e244e6d260e2daefd2ae04
sysnote8main <sysnote8main@gmail.com>
2025-05-30 21:16:02 +0900

    fix: work process resources correctly

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,14 @@ plugins {
     alias(libs.plugins.lombok)
 }
 
+group = "net.azisaba"
+version = "4.5.1"
+description = "RyuZUPluginChat"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val orgName: String by project
+val repoUrl: String by project
+
 repositories {
     mavenCentral()
     maven("https://oss.sonatype.org/content/groups/public/")
@@ -25,11 +33,6 @@ dependencies {
     compileOnly(libs.lunachatplus)
 }
 
-group = "net.azisaba"
-version = "4.5.1"
-description = "RyuZUPluginChat"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
 publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
@@ -44,11 +47,27 @@ tasks.withType<Javadoc> {
     options.encoding = "UTF-8"
 }
 
-lombok {
-    version = "1.18.38"
+tasks.processResources {
+    val props =
+        mapOf(
+            "name" to name,
+            "version" to version,
+            "description" to description,
+            "orgName" to orgName,
+            "url" to repoUrl,
+        )
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
 }
 
 tasks.shadowJar {
     isEnableRelocation = true
     relocationPrefix = "net.azisaba.ryuzupluginchat.dependency"
+}
+
+lombok {
+    version = "1.18.38"
 }
