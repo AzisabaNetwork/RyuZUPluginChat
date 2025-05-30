756daa21b6236120e7134f9c1889b4812c7e3015
sysnote8main <sysnote8main@gmail.com>
2025-05-30 21:33:45 +0900

    feat: update actions and pin action version by pinact

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,12 +33,6 @@ dependencies {
     compileOnly(libs.lunachatplus)
 }
 
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
-}
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
@@ -74,4 +68,31 @@ tasks.shadowJar {
 
 lombok {
     version = "1.18.38"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            artifact(tasks.jar)
+        }
+    }
+
+    repositories {
+        maven {
+            name = "azisaba-repo"
+            credentials {
+                username = System.getenv("REPO_USERNAME")
+                password = System.getenv("REPO_PASSWORD")
+            }
+            url =
+                if (project.version.toString().endsWith("-SNAPSHOT")) {
+                    uri("https://repo.azisaba.net/repository/maven-snapshots/")
+                } else {
+                    uri("https://repo.azisaba.net/repository/maven-releases/")
+                }
+        }
+    }
 }
