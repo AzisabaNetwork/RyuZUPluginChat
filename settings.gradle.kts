77c2e617d3168ace70f7be1a5e7076943ddd9473
sysnote8main <sysnote8main@gmail.com>
2025-06-08 12:45:37 +0900

    feat: add settings for develocity to use buildScan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "RyuZUPluginChat"
+
+plugins {
+    id("com.gradle.develocity") version("4.0.2")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
